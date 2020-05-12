7bd9f62e1ae399e87cd959257afd6f14371ced1f
baszer <basfvega26@gmail.com>
2020-05-12 12:32:47 +0200

    Opdracht

---
+++ 2-Variabelen-en-datatype/taak03/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $melk = '1.99 Euro';
+
+        $leeftijd = '16 jaar';
+
+        echo 'De prijs van melk is ' . $melk  . ' Mijn leeftijd is' . $leeftijd
+    
+        
+    ?>
+</body>
+</html>
