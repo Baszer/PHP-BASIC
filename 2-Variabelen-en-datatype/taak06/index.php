87e4dc04fbc93718c5d831bb60ad13f9daef3708
baszer <basfvega26@gmail.com>
2020-05-13 11:37:32 +0200

    .

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        
+        
+        $voornaam = "Tom";
+        echo gettype($voornaam);
+        
+        $getal = 3;
+        echo gettype($getal); 
+        
+        $leuk = 0.1;
+        echo gettype($leuk);
+
+        $boolean = true;
+        echo gettype($boolean);
+
+        $null = null;
+        echo gettype($null);
+    
+    
+    ?>
+</body>
+</html>
