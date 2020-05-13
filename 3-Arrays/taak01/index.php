bd111f246541348bb9d41b5a826ae489bfebe2a6
baszer <basfvega26@gmail.com>
2020-05-13 12:16:02 +0200

    .

---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php   
+
+$myMovies = ["Back to the Future", "The Matrix", "The Joker"];
+
+echo "My fav movies are " . $myMovies[0] . ", " . $myMovies[1] . " and " . $myMovies[2] . "."; 
+
+?>
+
+</body>
+</html>
