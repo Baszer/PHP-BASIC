fbc4b5d24d31c18a8fbefc48640796b60294fcf0
baszer <basfvega26@gmail.com>
2020-05-13 11:51:57 +0200

    f

---
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php   
+
+$voornaam1 = "Justin";   
+$voornaam2 = "Sander";
+$voornaam3 = "Max";
+
+
+echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+
+
+?>
+
+</body>
+</html>
+
