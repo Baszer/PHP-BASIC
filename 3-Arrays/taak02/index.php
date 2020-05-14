18f136ad9a1415736c24fc8bcdff5379cd2ed335
baszer <basfvega26@gmail.com>
2020-05-14 23:32:37 +0200

    .

---
+++ 3-Arrays/taak02/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php   
+
+
+$myDogNames =  ['hond1', 'hond2', 'hond3'];
+$myDogNames[1] = 'dieenehond';
+
+
+echo $myDogNames[2]
+
+?>
+
+</body>
+</html>
