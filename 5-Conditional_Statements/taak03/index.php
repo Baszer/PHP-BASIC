8cdff7ecdfc52bd3f56921ed413c72d7fa77169d
baszer <basfvega26@gmail.com>
2020-06-15 15:41:55 +0200

    .

---
+++ 5-Conditional_Statements/taak03/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$variabele1 = 10;
+$variabele2 = 6;
+
+if($variabele1 == $variabele2) {
+    echo "Is gelijk";
+}
+
+else echo "Ongelijk!";
+
+
+$variabele3 = 20;
+$variabele4 = 20;
+
+if($variabele3 == $variabele4) {
+    echo "Is identiek!";
+}
+
+
+
+$variabele5 = 40;
+$variabele6 = 10;
+
+if($variabele5 > $variabele6) {
+    echo "Test 1 is groter dan test 2!";
+}
+
+
+$variabele7 = 20;
+$variabele8 = 20;
+
+if($variabele7 <> $variabele8) {
+    echo "Is groter of gelijk aan test 2!";
+}
+
+
+
+?>
+
+
+
+
+</body>
+</html>
